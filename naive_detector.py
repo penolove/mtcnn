4d746672273592d5438dab41c3379a832243810c
penolove <chinghua.yang@appier.com>
2019-03-12 02:21:38 +0800

    style clean for demo.py

---
+++ naive_detector.py
@@ -0,0 +1,23 @@
+import arrow
+
+from eyewitness.image_id import ImageId
+from eyewitness.image_utils import ImageHandler, Image
+from bistiming import SimpleTimer
+
+
+def get_face_detector(model):
+    pass
+
+
+if __name__ == '__main__':
+    model_name = 'MobileNet'
+    with SimpleTimer("Loading model %s" % model_name):
+        object_detector = get_face_detector(model_name)
+    raw_image_path = 'demo/test_image.jpg'
+    image_id = ImageId(channel='demo', timestamp=arrow.now().timestamp, file_format='jpg')
+    image_obj = Image(image_id, raw_image_path=raw_image_path)
+    with SimpleTimer("Predicting image with classifier"):
+        detection_result = object_detector.detect(image_obj)
+    print("detected %s objects" % len(detection_result.detected_objects))
+    ImageHandler.draw_bbox(image_obj.pil_image_obj, detection_result.detected_objects)
+    ImageHandler.save(image_obj.pil_image_obj, "detected_image/drawn_image.jpg")
